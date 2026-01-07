3feeedfa147c49fc9f86985d93d724e491ea2776
chetankhadse21 <chetankhadse25@gmail.com>
2026-01-07 17:31:52 +0530

    connecting registrations to database

--- backend/jwtauth/jwtauth/settings.py
+++ backend/jwtauth/jwtauth/settings.py
@@ -89,10 +89,15 @@ WSGI_APPLICATION = 'jwtauth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'chetan@2005-21/',
+        'HOST': 'db.cmltqhabkknzvorptjjk.supabase.co',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
